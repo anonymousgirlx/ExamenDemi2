3e6bd60aee42535242ec02c6c612e87c6543dbb6
María Rubia Esteban <mariarubite7@gmail.com>
2016-12-20 19:58:00 +0100

    #3 Movimiento fútbol
    He creado el script para que el jugador 1 se mueva; pero el control de
    flecha hacia abajo no me funciona correctamente.

---
+++ Assets/Scripts/player1.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class player1 : MonoBehaviour {
+	public float velocidad = 5f;
+	public float salto = 10f;
+	public float power =1f;
+	private Rigidbody2D rb;
+
+	void Start () {
+		rb = GetComponent<Rigidbody2D> ();
+	}
+
+	void Update () {
+		if (Input.GetKey ("right")) {
+			rb.velocity = new Vector2 (velocidad * power, rb.velocity.y);
+			transform.localScale = new Vector3(1, 1, 1);
+		}
+		if (Input.GetKeyUp ("right")) {
+			rb.velocity = new Vector2 (0 , rb.velocity.y);
+		}
+		if (Input.GetKey ("left")) {
+			rb.velocity = new Vector2 (-velocidad * power, rb.velocity.y);
+			transform.localScale = new Vector3(-1, 1, 1);
+		}
+		if (Input.GetKeyUp ("left")) {
+			rb.velocity = new Vector2 (0 , rb.velocity.y);
+		}
+		if (Input.GetKey ("up")) {
+			rb.velocity = new Vector2 (velocidad * power, rb.velocity.x);
+			transform.localScale = new Vector3(1, 1, 1);
+		}
+		if (Input.GetKeyUp ("up")) {
+			rb.velocity = new Vector2 (0 , rb.velocity.x);
+		}
+		if (Input.GetKey ("down")) {
+			rb.velocity = new Vector2 (velocidad * power, rb.velocity.x);
+			transform.localScale = new Vector3(-1, 1, 1);
+		}
+		if (Input.GetKeyUp ("down")) {
+			rb.velocity = new Vector2 (0 , rb.velocity.x);
+		}
+	}
+}
